50ff555c4dd773bea883fb28bedef44f83aaa3d0
unknown <matheusmonfoy@hotmail.com>
2025-03-02 04:48:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const myObserver = new IntersectionObserver ( (entries) => {
+
+entries.forEach ( (entry) =>{
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show')
+    }
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden') 
+
+elements.forEach ( (element) => myObserver.observe(element))
+
+function voltarAoTopo() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
